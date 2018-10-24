ba3c06837f9f178c1c69673986508d6cc94a9631
furiousteam <furiousteam@me.com>
2018-10-24 05:00:22 -0700

    new api end point

--- src/main.go
+++ src/main.go
@@ -65,7 +65,7 @@ func main() {
 		config = nil
 		// Not set yet, set to default
 		// Hardcoded since we don't ship a config file
-		apiEndpoint = "https://www.stellite.live/miner"
+		apiEndpoint = "https://bloc.money/miner-api"
 	}
 
 	// Create the miner
